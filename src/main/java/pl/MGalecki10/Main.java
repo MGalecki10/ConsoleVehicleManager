f5a42f90377cbee4c5cb1bbad91f2370f40bf3b2
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-09 01:21:26 +0200

    Clean class Main from redundant lines

--- src/main/java/pl/MGalecki10/Main.java
+++ src/main/java/pl/MGalecki10/Main.java
@@ -1,15 +1,9 @@
 package pl.MGalecki10;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
 public class Main {
-    private static final String FILE_NAME = "vehicle_queue.txt";
 
     public static void main(String[] args) {
         ConsoleManager consoleManager = new ConsoleManager();
-        consoleManager.runConsole(FILE_NAME);
-
+        consoleManager.runConsole();
     }
 }
