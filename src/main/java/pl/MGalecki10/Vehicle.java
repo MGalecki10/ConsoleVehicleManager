abfe08311d48c211d5eb2fdf4246246f91dbd980
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-08 23:47:41 +0200

    Add Vehicle

---
+++ src/main/java/pl/MGalecki10/Vehicle.java
@@ -0,0 +1,78 @@
+package pl.MGalecki10;
+
+public class Vehicle {
+    private String type;
+    private String brand;
+    private String model;
+    private int year;
+    private double mileage;
+    private String vinNumber;
+
+    public Vehicle(String type, String brand, String model, int year, double mileage, String vinNumber) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.vinNumber = vinNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVinNumber() {
+        return vinNumber;
+    }
+
+    public void setVinNumber(String vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "type='" + type +
+                "', brand='" + brand +
+                "', model='" + model +
+                "', year=" + year +
+                ", mileage=" + mileage +
+                ", vinNumber='" + vinNumber +
+                "'\n";
+    }
+}
